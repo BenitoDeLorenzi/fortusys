baf7717b347c6e8ead7d3df6a9fcdc847f51c325
Benito De Lorenzi <benitodelorenzi@gmail.com>
2024-03-11 11:17:18 -0300

    Adicionando arquivos iniciais

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 //export const apiInterServ = "https://fortusysapi.com:5000/";
-export const apiInterServ = "http://localhost:5000/";
+export const apiInterServ = "http://54.237.161.243:5000/";
 
 export const sleep = async (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
